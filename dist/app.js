10c536ff5e5d6714f1f8b500099dda7687a7a38f
david-stefan-hermann <david.stefan.hermann@gmail.com>
2025-01-10 22:41:50 +0100

    base structure

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NoteService_1 = require("./services/NoteService");
-if (typeof document !== 'undefined') {
-    const noteService = new NoteService_1.NoteService();
-    noteService.initialize();
-}
